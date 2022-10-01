e3ec4ad3e16ccad0a12aac1e22e1aa4e6945e505
brinkqiang <brink.qiang@gmail.com>
2022-10-02 00:55:31 +0800

    add

--- thirdparty/dmpybind11/cmake/PythonModuleImport.cmake
+++ thirdparty/dmpybind11/cmake/PythonModuleImport.cmake
@@ -1,55 +1,52 @@
 
 macro(SUBDIRLIST result curdir)
-    FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
-    SET(dirlist "")
-    FOREACH(child ${children})
-        IF(IS_DIRECTORY ${curdir}/${child})
-            LIST(APPEND dirlist ${child})
-        ENDIF()
-    ENDFOREACH()
-    SET(${result} ${dirlist})
+FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
+SET(dirlist "")
+FOREACH(child ${children})
+    IF(IS_DIRECTORY ${curdir}/${child})
+        LIST(APPEND dirlist ${child})
+    ENDIF()
+ENDFOREACH()
+SET(${result} ${dirlist})
 endmacro()
 
 macro(PyModuleImport ModulePath DependsLib)
-    MESSAGE(STATUS "PyModuleImport ${ModulePath} ${DependsLib}")
+MESSAGE(STATUS "PyModuleImport ${ModulePath} ${DependsLib}")
 
-    IF (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath})
-        SUBDIRLIST(SUBDIRS ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath})
-        FOREACH(subdir ${SUBDIRS})
-            MESSAGE(STATUS "INCLUDE -> ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}")
-            INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir})
-            FILE(GLOB_RECURSE BIN_SOURCES
-            ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.cpp
-            ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.cc
-            ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.c
-            ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.hpp
-            ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.h)
+IF (IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath})
+    SUBDIRLIST(SUBDIRS ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath})
+    FOREACH(subdir ${SUBDIRS})
+        MESSAGE(STATUS "INCLUDE -> ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}")
+        INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir})
+        FILE(GLOB_RECURSE BIN_SOURCES
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.cc
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.c
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.hpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/*.h)
 
-            LIST(FILTER BIN_SOURCES EXCLUDE REGEX "${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/tpl/*")
+        LIST(FILTER BIN_SOURCES EXCLUDE REGEX "${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/tpl/*")
 
-            MESSAGE(STATUS "BIN_SOURCES ${LIB_SOURCES}")
+        MESSAGE(STATUS "BIN_SOURCES ${LIB_SOURCES}")
 
-            SET(LIB_SUFFIX "${CMAKE_SHARED_LIBRARY_SUFFIX}")
-            SET(CMAKE_SHARED_LIBRARY_SUFFIX ".pyd")
+        ADD_LIBRARY(${subdir} SHARED ${BIN_SOURCES})
+        TARGET_LINK_LIBRARIES(${subdir} ${DependsLib})
 
-            MESSAGE(STATUS "change suffix : ${LIB_SUFFIX} --> ${CMAKE_SHARED_LIBRARY_SUFFIX}")
+        IF (UNIX OR APPLE)
+          set_property (TARGET ${subdir} PROPERTY PREFIX "")
+        ELSEIF(WIN32)
+          set_property (TARGET ${subdir} PROPERTY SUFFIX ".pyd")
+        ENDIF()
 
-            ADD_LIBRARY(${subdir} SHARED ${BIN_SOURCES})
-            TARGET_LINK_LIBRARIES(${subdir} ${DependsLib})
+        MESSAGE(STATUS $<TARGET_FILE:${subdir}>)
+        ADD_CUSTOM_COMMAND(
+        TARGET ${subdir} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ModulePath}/${subdir}/${subdir}.py
+        $<TARGET_FILE_DIR:${subdir}>
+        )
 
-            MESSAGE(STATUS "change suffix : ${CMAKE_SHARED_LIBRARY_SUFFIX} --> ${LIB_SUFFIX}")
-            SET(CMAKE_SHARED_LIBRARY_SUFFIX "${LIB_SUFFIX}")
-
-            MESSAGE(STATUS "$<TARGET_FILE_DIR:${subdir}>")
-            MESSAGE(STATUS $<TARGET_FILE:${subdir}>)
-            ADD_CUSTOM_COMMAND(
-            TARGET ${subdir} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            $<TARGET_FILE:${subdir}>
-            $<TARGET_FILE_DIR:${subdir}>/${subdir}.pyd
-            )
-
-        ENDFOREACH()
-    ENDIF()
+    ENDFOREACH()
+ENDIF()
 
 endmacro(PyModuleImport)
