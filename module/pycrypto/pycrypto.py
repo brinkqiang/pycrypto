d5506976d08d616f790a403aa187945dae79ecb7
brinkqiang <brink.qiang@gmail.com>
2024-05-23 17:50:47 +0800

    add

--- module/pycrypto/pycrypto.py
+++ module/pycrypto/pycrypto.py
@@ -8,6 +8,11 @@ print(enbuf)
 debuf = rc.Decrypt(enbuf)
 print(debuf)
 
-
 md5 = pycrypto.CDMMD5()
 print(md5.GetMD5("hello world"))
+
+base64 = pycrypto.CDMBase64()
+base64_encode = base64.Base64Encode("hello world")
+print(base64_encode)
+base64_decode = base64.Base64Decode(base64_encode)
+print(base64_decode)
